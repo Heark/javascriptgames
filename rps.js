091824c78af16c98a52aef8ecf0dfa49870ff896
Heark <hearkofsorcerey@gmail.com>
2014-09-29 11:58:34 -0400

    Update rps.js

--- rps.js
+++ rps.js
@@ -1,3 +1,10 @@
-/****************************************************************
-/ Rock Paper Scissors Game
-/****************************************************************
+var choice = prompt("Rock, Paper, or Scissors?")
+var items = ["Rock","Paper","Scissors"]
+var compchoice = items[Math.floor(Math.random()*items.length)];
+if (choice == "Rock"){
+  if (compchoice == "Rock"){
+  console.log("You chose Rock and computer chose Rock. TIE GAME!")
+  else if (compchoice == "Paper")
+      console.log("You Lose!")
+}
+}
